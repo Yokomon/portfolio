1bb69de80c678c216763443eca9945f1d46b6f07
yokomon <marowmars@gmail.com>
2023-04-22 23:20:00 +0100

    🏷️ Schemas for projects setup

---
+++ types/Project.ts
@@ -0,0 +1,11 @@
+import { PortableTextBlock } from "sanity";
+
+export interface Project {
+  _id: string;
+  _createdAt: Date;
+  name: string;
+  slug: string;
+  image: string;
+  url: string;
+  content: PortableTextBlock[];
+}
