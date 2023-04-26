4fb5e104cb046a85d04e710d5bb5ed1fc856f4b4
yokomon <marowmars@gmail.com>
2023-04-26 23:58:09 +0100

    ✨ Added Animated text component

---
+++ src/components/common/AnimatedText.tsx
@@ -0,0 +1,65 @@
+import { motion } from "framer-motion";
+
+interface AnimatedTextProps {
+  text: string;
+  className?: string;
+  textWrapper?: string;
+}
+
+const quote = {
+  initial: {
+    opacity: 1,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const singleWord = {
+  initial: {
+    opacity: 0,
+    y: 50,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1.5,
+    },
+  },
+};
+
+const AnimatedText: React.FC<AnimatedTextProps> = ({
+  text,
+  className = "",
+  textWrapper,
+}) => {
+  return (
+    <div
+      className={`w-full mx-auto overflow-hidden flex justify-center items-center py-2 ${textWrapper}`}
+    >
+      <motion.h1
+        variants={quote}
+        initial="initial"
+        animate="animate"
+        className={`inline-block w-full text-dark font-bold capitalize text-5xl ${className}`}
+      >
+        {text.split(" ").map((word, idx) => (
+          <motion.span
+            variants={singleWord}
+            key={word + "_" + idx}
+            className={"inline-block"}
+          >
+            {word}&nbsp;
+          </motion.span>
+        ))}
+      </motion.h1>
+    </div>
+  );
+};
+
+export default AnimatedText;
