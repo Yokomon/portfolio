a1175a420a05dd8fe25ec0cc9335184319cd0486
yokomon <marowmars@gmail.com>
2023-05-01 17:32:24 +0100

    🏷️ Types: About page types

---
+++ types/About.ts
@@ -0,0 +1,56 @@
+import { IconType } from 'react-icons'
+import { PortableTextBlock } from 'sanity'
+
+export interface AnimatedNumbersProps {
+  value: number
+}
+
+export interface Skill {
+  name: string
+  x?: string
+  y?: string
+  center?: boolean
+  icon?: IconType
+  iconClass?: string
+}
+
+enum StatTypes {
+  Client = 'clients',
+  Projects = 'projects',
+  Years = 'years',
+}
+
+type Stats = {
+  _key: string
+  _type: StatTypes.Client | StatTypes.Projects | StatTypes.Years
+  value: number
+}
+
+export type Experience = {
+  workSummary: PortableTextBlock[]
+  duration: string
+  workTitle: string
+  company: string
+  companyUrl: string
+  companyLocation: string
+}
+
+export interface AboutData {
+  _id: string
+  _createdAt: Date
+  name: string
+  darkImage: string
+  lightImage: string
+  biography: PortableTextBlock[]
+  stats: Stats[]
+  experience: Experience[]
+}
+
+export interface Details {
+  position: string
+  company: string
+  companyLink: string
+  duration: string
+  address: string
+  work: PortableTextBlock[]
+}
