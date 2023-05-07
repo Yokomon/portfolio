9435e844b92d1408bf8bdbed2f2d0ab0bd08a6d5
yokomon <marowmars@gmail.com>
2023-05-07 22:07:59 +0100

    ♻️ Refactor for articles schema

--- sanity/schemas/article-schemas.ts
+++ sanity/schemas/article-schemas.ts
@@ -1,44 +1,44 @@
-import { SanityDocs } from "./types";
+import { SanityDocs } from './types'
 
 const article: SanityDocs = {
-  name: "articles",
-  title: "Articles",
-  type: "document",
+  name: 'articles',
+  title: 'Articles',
+  type: 'document',
   fields: [
     {
-      name: "name",
-      title: "Name",
-      type: "string",
+      name: 'name',
+      title: 'Name',
+      type: 'string',
     },
     {
-      name: "slug",
-      title: "Slug",
-      type: "slug",
-      options: { source: "name" },
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'name' },
     },
     {
-      name: "image",
-      title: "Image",
-      type: "image",
+      name: 'image',
+      title: 'Image',
+      type: 'image',
       options: { hotspot: true },
-      fields: [{ name: "alt", title: "Alt", type: "string" }],
+      fields: [{ name: 'alt', title: 'Alt', type: 'string' }],
     },
     {
-      name: "url",
-      title: "Url",
-      type: "url",
+      name: 'url',
+      title: 'Url',
+      type: 'url',
     },
     {
-      name: "summary",
-      title: "Summary",
-      type: "string",
+      name: 'summary',
+      title: 'Summary',
+      type: 'string',
     },
     {
-      name: "duration",
-      title: "Duration",
-      type: "string",
+      name: 'duration',
+      title: 'Duration',
+      type: 'string',
     },
   ],
-};
+}
 
-export default article;
+export default article
