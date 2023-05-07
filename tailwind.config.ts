6ca1bc2ff04d2d4fbd0b11d42de2917e75ab129f
yokomon <marowmars@gmail.com>
2023-05-07 22:07:33 +0100

    💄 Update styles in config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #fff 100px)',
+        circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #fff 98px)',
         circularDark:
           'repeating-radial-gradient(rgba(255,255,255,0.5) 2px, #0F172A 8px, #ffffff47 250px)',
       },
