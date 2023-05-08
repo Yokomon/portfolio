8e32f2e2f5a06e1592e3d29d6b48715613b5da46
yokomon <marowmars@gmail.com>
2023-05-08 23:29:46 +0100

    🔧 Update sanity config

--- sanity/config/client-config.tsx
+++ sanity/config/client-config.tsx
@@ -1,5 +1,8 @@
-export const clientConfig = {
+import { createClient } from 'next-sanity'
+
+export const clientConfig = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-};
+  useCdn: true,
+})
