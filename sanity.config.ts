0f87060a01bedc50cf4259e3ad3010453cd6ec9c
yokomon <marowmars@gmail.com>
2023-05-16 23:35:22 +0100

    🔧 Updated my config file

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-import schemas from "@/sanity/schemas";
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { codeInput } from '@sanity/code-input'
+import schemas from '@/sanity/schemas'
 
 const config = defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_ID as string,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
   title: process.env.NEXT_PUBLIC_SANITY_TITLE,
   basePath: process.env.NEXT_PUBLIC_SANITY_BASE_PATH,
-  plugins: [deskTool()],
+  plugins: [deskTool(), codeInput()],
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   schema: { types: schemas },
-});
+})
 
-export default config;
+export default config
